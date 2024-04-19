da9b08e818b342648e3d8c6edc334027fdea5365
Fleshypi <162758195+Fleshypi@users.noreply.github.com>
2024-04-19 13:19:33 -0500

    Added example tas

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from machine import Pin
+import time
 
+## Frame rate ##
+
+framerate = 60
+frame = 1/framerate
+count = 0
+
+
+tas = ["up","down","up","down","left","right","left","right","b","a","start","select"]
+
+while count <= len(tas):
+	Pin(tas[count]).toggle()
+	count += 1
+	time.sleep(frame)
